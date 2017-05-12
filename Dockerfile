447a7837760a26c508eea12a1ecc511a704303e8
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2017-05-11 22:32:43 -0700

    more docker setup

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip install -r requirements.txt
 
 COPY . /app
 
-CMD python flask-compose.py
+CMD python home.py
--- home.py
+++ home.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World with Docker!'
 
 
 if __name__ == '__main__':
-    app.run()
+    # This is here so we can access the web app from outside the container
+    app.run(host='0.0.0.0')
