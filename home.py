3c35ef8a0744d1983365455d1edf4b3052afbe3b
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2017-05-13 11:45:43 -0700

    got the docker stuff working in pycharm.
    i had to use docker-compose.override instead of .dev to mount my app

--- home.py
+++ home.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World with Docker!'
+    return 'Hello World with Docker!!!!!gfjkighjdfgh'
 
 
 if __name__ == '__main__':
     # This is here so we can access the web app from outside the container
-    app.run(host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
